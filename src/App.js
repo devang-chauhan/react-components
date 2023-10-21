ebb0fe51e79e696e072468904329a257ae75f908
Devang <devngc@gmail.com>
2023-10-20 22:01:05 -0400

    fix: badge

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import Badge from './Components/Badge';
 
 function App() {
   return (
-    <div className="App">
-      <Badge shape="pill" color="purple">Badge</Badge>
+    <div className="app">
+      <div className="badges">
+        <Badge color="red">Badge</Badge>
+        <Badge color="yellow">Badge</Badge>
+        <Badge color="green">Badge</Badge>
+        <Badge color="blue">Badge</Badge>
+        <Badge color="indigo">Badge</Badge>
+        <Badge color="purple">Badge</Badge>
+      </div>
+
+      <div className="badges">
+        <Badge shape="pill" color="red">Badge</Badge>
+        <Badge shape="pill" color="yellow">Badge</Badge>
+        <Badge shape="pill" color="green">Badge</Badge>
+        <Badge shape="pill" color="blue">Badge</Badge>
+        <Badge shape="pill" color="indigo">Badge</Badge>
+        <Badge shape="pill" color="purple">Badge</Badge>
+      </div>
+     
     </div>
   );
 }
--- src/Components/Badge.js
+++ src/Components/Badge.js
@@ -1,9 +1,7 @@
-
-
 export default function Badge({shape, color, children, ...rest}) { 
 
     return (
-        <div className={`badge ${shape.toLowerCase() === 'pill' ? `pill` : ''} ${color}`}
+        <div className={`badge ${shape && shape.toLowerCase() === 'pill' ? `pill` : ''} ${color}`}
             {...rest}>
             {children}
         </div>
